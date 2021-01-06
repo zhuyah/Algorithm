322f4cc7ddc7bb5059295665cb8f43d6e8f349ab
zhuyah <zhuyah@foxmail.com>
2021-01-06 22:56:19 +0800

    dp

--- src/com/algorithm/leetcode/LeetCode_0005.java
+++ src/com/algorithm/leetcode/LeetCode_0005.java
@@ -15,11 +15,39 @@ package com.algorithm.leetcode;
  **/
 public class LeetCode_0005 {
     public static void main(String[] args) {
-
+        System.out.println(longestPalindrome("ac"));
     }
 
     public static String longestPalindrome(String s) {
+        int maxLen = 1, index = 0;
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        boolean[][] dp = new boolean[len][len];
 
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = true;
+        }
 
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i < 3) {          //j-1-(i+1)+1<2
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    index = i;
+                }
+            }
+        }
+        return s.substring(index, index + maxLen);
     }
 }
